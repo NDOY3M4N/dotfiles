c84a56f0910288f87a90184c742816079f3a02c1
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2024-09-10 21:46:53 +0000

    nvim: ensure more parser are installed
    Also disabled the `vim-matchup` for now, it was causing a bug on the
    Svelte LSP

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -3,13 +3,13 @@ return {
   'nvim-treesitter/nvim-treesitter',
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
-    {
-      'andymass/vim-matchup',
-      event = 'CursorMoved',
-      config = function()
-        vim.g.matchup_matchparen_offscreen = { method = 'popup' }
-      end,
-    },
+    -- {
+    --   'andymass/vim-matchup',
+    --   event = 'CursorMoved',
+    --   config = function()
+    --     vim.g.matchup_matchparen_offscreen = { method = 'popup' }
+    --   end,
+    -- },
   },
   build = ':TSUpdate',
   lazy = false,
