b082ce3bc238f4ec40cfe1b154283f0ab1c7b277
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2024-09-20 20:12:22 +0000

    nvim: add go snippets

---
+++ nvim/lua/custom/config/snips/go.lua
@@ -0,0 +1,34 @@
+require('luasnip.session.snippet_collection').clear_snippets 'go'
+
+local ls = require 'luasnip'
+
+local s = ls.snippet
+local i = ls.insert_node
+local fmt = require('luasnip.extras.fmt').fmt
+
+ls.add_snippets('go', {
+  s(
+    'fm',
+    fmt(
+      [[
+func (<>) <>(<>) <> {
+  <>
+}
+]],
+      { i(1), i(2), i(3), i(4), i(0) },
+      { delimiters = '<>' }
+    )
+  ),
+  s(
+    'ff',
+    fmt(
+      [[
+func <>(<>) <> {
+  <>
+}
+]],
+      { i(1), i(2), i(3), i(0) },
+      { delimiters = '<>' }
+    )
+  ),
+})
