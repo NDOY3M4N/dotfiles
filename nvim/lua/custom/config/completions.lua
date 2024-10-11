879142bf9f505e8f02250e4a2c42c7692a4ffce1
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2024-10-11 10:18:23 +0000

    refac: change keymaps for completions
    Set the keymap for accepting Luasnip completions to be the same as my
    `fzf` keymap.

--- nvim/lua/custom/config/completions.lua
+++ nvim/lua/custom/config/completions.lua
@@ -7,7 +7,7 @@ local cmp = require 'cmp'
 
 cmp.setup {
   sources = {
-    { name = 'cody' },
+    -- { name = 'cody' },
     { name = 'luasnip' },
     { name = 'git' },
     { name = 'nvim_lsp' },
@@ -16,10 +16,10 @@ cmp.setup {
   },
   mapping = {
     ['<C-Space>'] = cmp.mapping.complete {},
-    ['<C-e>'] = cmp.mapping.close(),
+    ['<C-y>'] = cmp.mapping.close(),
     ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
     ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-    ['<C-y>'] = cmp.mapping(
+    ['<C-e>'] = cmp.mapping(
       cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Insert,
         select = true,
