92c694fa10e5a76c34d41f1850c482b1eccaf59d
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2024-11-11 11:09:18 +0000

    nvim: try lspkind once again
    Hope this time it'll be better

--- nvim/lua/custom/config/completions.lua
+++ nvim/lua/custom/config/completions.lua
@@ -4,6 +4,7 @@ vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vim.opt.shortmess:append 'c'
 
 local cmp = require 'cmp'
+local lspkind = require 'lspkind'
 
 cmp.setup {
   sources = {
@@ -35,6 +36,20 @@ cmp.setup {
       -- NOTE: we can also use native snippet utility (in Neovim 0.10+)
       -- vim.snippet.expand(args.body)
     end,
+  },
+  formatting = {
+    format = lspkind.cmp_format {
+      mode = 'symbol',
+      maxwidth = {
+        menu = 50,
+        abbr = 50,
+      },
+      ellipsis_char = '...',
+      show_labelDetails = true,
+      symbol_map = { Cody = '  ' },
+    },
+    fields = { 'abbr', 'kind', 'menu' },
+    expandable_indicator = true,
   },
   -- experimental = {
   --   ghost_text = true,
--- nvim/lua/custom/plugins/completions.lua
+++ nvim/lua/custom/plugins/completions.lua
@@ -11,6 +11,7 @@ return {
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-buffer',
       'petertriho/cmp-git',
+      'onsails/lspkind.nvim',
       -- Snippet Engine & its associated nvim-cmp source
       { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp' },
     },
