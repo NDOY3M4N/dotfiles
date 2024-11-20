ee321cd9fef5ccb69fc12139b183f68f9cd52070
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2024-11-20 08:45:00 +0000

    feat: add twilight to nvim & integrate with kitty

---
+++ nvim/lua/custom/plugins/twilight.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+return {
+  {
+    'folke/twilight.nvim',
+    config = true,
+    keys = {
+      { '<leader>tt', '<CMD>Twilight<CR>', desc = 'Toggle Twilight' },
+    },
+  },
+}
